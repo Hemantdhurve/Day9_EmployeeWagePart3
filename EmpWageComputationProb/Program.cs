f578c7c03c949d75c8177474719a77534117fc56
Hemantdhurve <hemantdhurve007@gmail.com>
2022-08-15 15:14:31 +0530

    initial commit

---
+++ EmpWageComputationProb/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmpWageComputationProb
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Problem Part 3");
+        }
+    }
+}
