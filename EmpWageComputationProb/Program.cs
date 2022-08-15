c9f80b1ee6a8331c5a6dc7950a11d44d910daa20
Hemantdhurve <hemantdhurve007@gmail.com>
2022-08-15 15:39:51 +0530

    Added UC1 Employee Attendance file

--- EmpWageComputationProb/Program.cs
+++ EmpWageComputationProb/Program.cs
@@ -7,6 +7,13 @@ namespace EmpWageComputationProb
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation Problem Part 3");
+
+            Console.WriteLine("Check whether employee is Present or Absent");
+            //Creating Object of the class to call in Main method
+            //because method is non static
+
+            EmployeeWage emp1 = new EmployeeWage();
+            emp1.EmployeeCheckAttendance();
         }
     }
 }
