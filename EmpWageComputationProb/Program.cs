f16b2dff29dafa8ff4805cce1f9a1e45c928cebf
Hemantdhurve <hemantdhurve007@gmail.com>
2022-08-15 15:55:07 +0530

    Added UC3 Emp Wage Part time file

--- EmpWageComputationProb/Program.cs
+++ EmpWageComputationProb/Program.cs
@@ -8,12 +8,12 @@ namespace EmpWageComputationProb
         {
             Console.WriteLine("Welcome to Employee Wage Computation Problem Part 3");
 
-            Console.WriteLine("To find daily wage as a Output");
+            Console.WriteLine("To get Employee is Part time or Full time as a Output:");
             //Creating Object of the class to call in Main method
             //because method is non static
 
             EmployeeWage emp = new EmployeeWage();
-            emp.EmpDailyWage();
+            emp.EmpWagePartTime();
         }
     }
 }
