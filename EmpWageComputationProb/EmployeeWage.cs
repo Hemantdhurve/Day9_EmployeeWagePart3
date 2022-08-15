28e0d78cc163e982a56bdffd9ddca824d1d15d87
Hemantdhurve <hemantdhurve007@gmail.com>
2022-08-15 15:50:50 +0530

    Added UC2 Emp Daily Wage File

--- EmpWageComputationProb/EmployeeWage.cs
+++ EmpWageComputationProb/EmployeeWage.cs
@@ -8,10 +8,15 @@ namespace EmpWageComputationProb
 {
     public class EmployeeWage
     {
-        public void EmployeeCheckAttendance()
+        public void EmpDailyWage()
         {
             //define constants
             int IS_FULL_TIME = 1;
+            int Emp_Rate_Per_Hr = 20;
+
+            //variables
+            int empHrs = 0;
+            int empWage = 0;
 
             //random function is to create random numbers
             Random random = new Random();
@@ -21,12 +26,15 @@ namespace EmpWageComputationProb
 
             if (empCheck == IS_FULL_TIME)
             {
-                Console.WriteLine("Employee is present");
+                empHrs = 8;
             }
             else
             {
-                Console.WriteLine("Employee is absent");
+                empHrs = 0;
             }
+            empWage = empHrs * Emp_Rate_Per_Hr;
+            Console.WriteLine("Emp Wage :" + empWage);
         }
     }
+    
 }
--- EmpWageComputationProb/Program.cs
+++ EmpWageComputationProb/Program.cs
@@ -8,12 +8,12 @@ namespace EmpWageComputationProb
         {
             Console.WriteLine("Welcome to Employee Wage Computation Problem Part 3");
 
-            Console.WriteLine("Check whether employee is Present or Absent");
+            Console.WriteLine("To find daily wage as a Output");
             //Creating Object of the class to call in Main method
             //because method is non static
 
-            EmployeeWage emp1 = new EmployeeWage();
-            emp1.EmployeeCheckAttendance();
+            EmployeeWage emp = new EmployeeWage();
+            emp.EmpDailyWage();
         }
     }
 }
